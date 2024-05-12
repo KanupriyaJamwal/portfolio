4203f36e9f66d1d9e5434c460a64c8f5b325e082
KanupriyaJamwal <kanupriya15998kj@gmail.com>
2024-05-12 17:40:04 +0100

    updated about

--- src/About.js
+++ src/About.js
@@ -8,13 +8,10 @@ class About extends Component {
       <div>
         <section className="projects-wrapper projects-wrapper-about nested">
           <div>
-            <ul className="about-modules col-8 offset-1 nested">
-              <div className="about-group col-8 nested">
-                <li className="bio-text col-6 offset-1">
-                  <h1>
-                    HI! My name is <strong>*chika-chika*</strong> Kanupriya
-                    Jamwal
-                  </h1>
+            <ul className="about-modules col-10 nested">
+              <div className="about-group col-10 nested">
+                <li className="bio-text col-9 offset-1">
+                  <h1>HI! My name is Kanupriya Jamwal</h1>
                   <h2
                     style={{
                       textAlign: "center",
@@ -22,7 +19,7 @@ class About extends Component {
                       color: "#FF5783",
                     }}
                   >
-                    I like to watch movies.
+                    I am a pop-culture nerd.
                   </h2>
                 </li>
 
@@ -79,6 +76,64 @@ class About extends Component {
                 </ul>
               </div>
             </ul>
+          </div>
+        </section>
+        <section className="col-7 nested left-margin-120">
+          <div className="text-group">
+            <div className="large-paragraph medium-spacer-bottom col-2">
+              <h3>TV shows</h3>
+              <br></br>
+              <br></br>
+              <ul className="text-list medium-spacer-bottom col-2">
+                <li>You're The Worst</li>
+                <li>Fleabag</li>
+                <li>Broad City</li>
+              </ul>
+            </div>
+
+            <div className="large-paragraph medium-spacer-bottom col-2">
+              <h3>Movies</h3>
+              <br></br>
+              <br></br>
+              <ul className="text-list medium-spacer-bottom col-2">
+                <li>Gangs of Wasseypur</li>
+                <li>Heathers</li>
+                <li>In The Loop</li>
+              </ul>
+            </div>
+
+            <div className="large-paragraph medium-spacer-bottom col-2">
+              <h3>Manga</h3>
+              <br></br>
+              <br></br>
+              <ul className="text-list medium-spacer-bottom col-2">
+                <li>One Piece</li>
+                <li>Slam Dunk</li>
+                <li>Dorohedoro</li>
+              </ul>
+            </div>
+
+            <div className="large-paragraph medium-spacer-bottom col-2">
+              <h3>Podcasts</h3>
+              <br></br>
+              <br></br>
+              <ul className="text-list medium-spacer-bottom col-2">
+                <li>Trash Taste</li>
+                <li>Dead Ends</li>
+                <li>The Midnight Gospel</li>
+              </ul>
+            </div>
+
+            <div className="large-paragraph medium-spacer-bottom col-2">
+              <h3>Subcultures</h3>
+              <br></br>
+              <br></br>
+              <ul className="text-list medium-spacer-bottom col-2">
+                <li>Impressionism</li>
+                <li>Indie Rock</li>
+                <li>Engineers on YouTube</li>
+              </ul>
+            </div>
           </div>
         </section>
       </div>
